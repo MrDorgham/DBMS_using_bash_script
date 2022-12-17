9c600df80979044c21b09b3f60a1a5a9cc1260c8
Mohamed Dorgham <dorgham.inbox@gmail.com>
2022-12-17 02:01:35 +0200

    Renam_Table function test

--- mainDBMS.sh
+++ mainDBMS.sh
@@ -102,6 +102,7 @@ function Create_DataBase(){
         else
 
             mkdir ./.data_bases/$DBname 2>>./.error.log
+            mkdir ./.data_bases/$DBname/.tmptable 2>>./.error.log
             w8_clear
             successful_icon "new Database named $DBname has been successfuly created"
             main_Menu
@@ -244,6 +245,166 @@ function Connect_DataBase {
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
 #--------------------Tables functions----------------------#
 
+function Create_Table {
+    echo "Avilable tables are: " 
+
+    ls 
+    read -p "Enter table name you want to create : " TBName
+
+    wating_for_PK="true"
+
+    if  Regex_correct "$TBName" ; then
+
+        if [ $TBName ];
+        then
+            if [ -a $TBName ]; 
+                then
+                fail_icon "Table with name $TBName already exists"    
+            else
+                touch .tmptable/$TBname
+                read -p "Enter the number of coloumns : " COLNumber
+                
+                 expr $COLNumber + 1 2> /dev/null 
+
+                while [ $? != 0 ]
+                do
+                    warning_icon "Please enter a valid number"
+                    read -p "Enter the number of coloumns : " COLNumber
+                    expr $COLNumber + 1 2> /dev/null >> /dev/null
+
+                done
+
+                ## now read colomn type and names
+                i=1
+                while [ $i -le $COLNumber  ]
+                do
+                    ## gen name of field
+                    read -p "Enter the name of column number $i: " COLName;
+
+                    while [ -z "$COLNumber" ]; do
+
+                        warning_icon "Field name can't be empty";
+                        read -p "Enter the name of column number $i: " COLName;
+
+                    done
+
+                    ## check if field is PK or not
+                    while [ $wating_for_PK == "true" ]
+                    do         
+                        echo "is this feild a primary key?"
+                        select answer in "y" "n"
+                        do
+                            case $answer in
+                            "y" )
+                                echo -n "%P_Key%" >> .tmptable/$TBName
+                                wating_for_PK="false"
+                                break
+                                ;;
+                            "n" )
+                                break 
+                                ;; 
+                            * )
+                                warning_icon "Please choose a valid option"
+                                ;;
+                            esac
+                        done 
+                        break
+                    done
+
+                    ## get datatype of field
+                    read -p "Enter a valid column $i datatype : [string/int] " data_Type;
+                    while [[ "$data_Type" != *(int)*(string) || -z $data_Type ]]
+                    do
+                        warning_icon "Invalid datatype"
+                        read -p "Enter a valid column $i datatype again : [string/int] " data_Type;
+                    done
+
+                    ## write in the table file; check to stop adding ":" in the last field
+                    if [ $i -eq $COLNumber ]
+                        then
+                        echo $COLName"%"$data_Type"%" >> .tmptable/$TBName
+                    else
+                        echo -n $COLName"%"$data_Type"%:" >> .tmptable/$TBName
+                    fi
+
+                    ((i=$i+1))
+
+                done
+                
+                mv .tmptable/$TBName ./$TBName
+                w8_clear
+                successful_icon "Table created successfully"    
+            fi
+        else
+            fail_icon "invalid input please enter a valid name"
+        fi
+        tables_Menu
+    else
+        Create_Table
+    fi
+
+
+
+
+
+
+}
+
+
+function Renam_Table {
+    if [ -z "$(ls -A )" ]; then
+
+        warning_icon "there is no tables to rename"
+        tables_Menu
+
+    else
+        echo "Avilable Tables Are: " 
+        ls
+        read -p "Enter the table name you want to rename it  : " old_TBname
+
+        if  Regex_correct "$old_TBname" ; then
+
+         x=`ls | grep "$old_TBname"` 
+
+            if [ $? -eq 0 ] ; then
+                read -p "Enter the new table name : " new_TBname
+
+                if Regex_correct "$new_TBname" ; then
+
+                    if [ -d $new_TBname ] ; then
+                        
+                     	fail_icon "DB with the same name $new_TBname already exists"
+                        echo " please re-enter a new name.."
+                        Renam_Table
+
+
+                    else
+                        w8_clear
+                        mv $old_TBname $new_TBname
+                        successful_icon "the $new_TBname has been successfuly renamed to $old_TBname"
+                        tables_Menu
+
+                    fi
+
+                else
+                    tables_Menu
+                fi
+
+            else 
+                fail_icon "$old_Name does not exist please try again"
+
+            fi
+
+        else
+            tables_Menu
+        fi
+
+    fi
+
+}
+
+
+
 function Drop_Table {
     if [ -z "$(ls -A )" ]; then
 
@@ -289,9 +450,6 @@ function Drop_Table {
 
 }
 
-
-
-
 #----------------------------------------------------------#
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#
 
@@ -299,29 +457,34 @@ function Drop_Table {
 
 
 function tables_Menu() {
-echo "   _____________________[Tables_Menu]____________________  "
+echo "   ________________[Tables creation Menu]________________  "
 echo "  /                                                      \ "
-echo "  @   1. Create New Table                                @ "
-echo "  @   2. Show Existing Tables                            @ "
-echo "  @   3. Drop Table                                      @ "
-echo "  @   4. Select From Table                               @ "
+echo "  @   1. Show Existing Tables                            @ "
+echo "  @   2. Create New Table                                @ "
+echo "  @   3. Rename Tables                                   @ "
+echo "  @   4. Drop Table                                      @ "
 echo "  @   5. Insert Into Table                               @ "
-echo "  @   5. Update From Table                               @ "
-echo "  @   6. Delete From Table                               @ "
-echo "  @   7. Back to the main menu                           @ "
-echo "  @   8. Exit                                            @ "
+echo "  @   6. Select From Table                               @ "
+echo "  @   7. Update From Table                               @ "
+echo "  @   8. Delete From Table                               @ "
+echo "  @   9. Back to the main menu                           @ "
+echo "  @   10. Exit                                           @ "
 echo "  \______________________________________________________/ "
   
   echo  
   read -p "Select option: "  user_input
   
   case $user_input in
-    1 )  Create_Table ;;
-    2 )  ls ; tables_Menu;;
-    3 )  Drop_Table ;;
-    4 )  Rename_Databases ;;
-    5 )  Drop_Databases ;;
-    6 )  exit ;;
+    1 )  ls ; tables_Menu;;
+    2 )  Create_Table ;;
+    3 )  Renam_Table ;;
+    4 )  Drop_Table ;;
+    5 )  Insert_Into_Table ;;
+    6 )  Select_From_Table ;;
+    7 )  Update_From_Table ;;
+    8 )  Delete_From_Table ;;
+    9 )  main_Menu ;;
+    10 )  exit ;;
     * )  warning_icon "Please select valid pick" ; tables_Menu ;
   esac
 
